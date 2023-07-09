50c38890c03661ad5ddb5882018c07e72515fad0
Dylan <109418123+WebbD777@users.noreply.github.com>
2023-07-09 13:22:55 +0200

    Website

---
+++ Javascript/index.js
@@ -0,0 +1,17 @@
+//let about = document.getElementById("aboutButton-id")
+//
+//
+function aboutButton(){
+    window.location.href = "about.html"
+    //window.open("/html/about.html")
+    //location.href = "html/about.html"
+}
+
+function goHome(){
+    window.location.href = "index.html"
+}
+
+function portfolioButton(){
+    window.location.href = "portfolio.html"
+}
+
